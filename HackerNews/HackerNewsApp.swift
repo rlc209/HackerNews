88ba96abc52370b3fa9757886c128850ac9d7df1
Randy Crossley <RandyLCrossley@gmail.com>
2021-06-18 03:16:50 -0400

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Randy Crossley on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
